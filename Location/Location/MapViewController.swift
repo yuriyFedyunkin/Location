5be3fe1d037739b03e7a6f06fb604e9682edf04c
Yuriy Fedyunkin <samnx39@gmail.com>
2021-10-08 10:24:14 +0200

    initial commit

---
+++ Location/Location/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  Location
+//
+//  Created by Yuriy Fedyunkin on 08.10.2021.
+//
+
+import UIKit
+
+final class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
